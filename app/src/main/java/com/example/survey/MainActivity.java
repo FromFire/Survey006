9151adb64f9af60eafe823580e57907826face64
user <yuanxiasummer@icloud.com>
2020-03-08 02:24:49 +0800

    test

--- app/src/main/java/com/example/survey/MainActivity.java
+++ app/src/main/java/com/example/survey/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.survey;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToSurvey(android.view.View V) {
+        CheckBox cb_accept = findViewById(R.id.accept);
+        if(!cb_accept.isChecked())
+            return;
+        Intent intent = new Intent(this, question_page.class);
+        startActivity(intent);
     }
 }
